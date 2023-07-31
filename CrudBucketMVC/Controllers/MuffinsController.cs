b726f27fd30ef7d8becfa42a6b04fb4c5a16386a
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2023-07-31 10:26:34 -0600

    Add Index View for Muffins

---
+++ CrudBucketMVC/Controllers/MuffinsController.cs
@@ -0,0 +1,21 @@
+﻿using CrudBucketMVC.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudBucketMVC.Controllers
+{
+    public class MuffinsController : Controller
+    {
+        private readonly CrudBucketContext _context;
+
+        public MuffinsController(CrudBucketContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var muffins = _context.Muffins.ToList();
+            return View(muffins);
+        }
+    }
+}
